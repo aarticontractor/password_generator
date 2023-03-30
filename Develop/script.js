9902e79a3566dc491b5f75ba2a67ff3364023c18
aarticontractor <contaaru@gmail.com>
2023-03-29 23:38:46 -0700

    added length and lowercase prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,43 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
-function writePassword() {
-    var my_new_password = "sagdyady3y1hdsuai";
-    passwordText.value = my_new_password;
+
+function generatePassword() {
+  var lengthPrompt = prompt("Enter password length (between 8 and 128):");
+  if (lengthPrompt === null || lengthPrompt === "") {
+    alert("Password length cannot be empty.");
+    return;
+  }
+  var length = parseInt(lengthPrompt);
+  
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Password length must be a number between 8 and 128.");
+    return;
+  }
+  var lowercasePrompt = prompt("Include lowercase letters? (yes/no)");
+  var includeLowercase = lowercasePrompt.toLowerCase() === "yes";
+  
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  
+  var allTypes = ""; 
+  // Include lowercase characters
+  if (includeLowercase) {
+    allTypes += lowercase;
+  }
+  
+
+
+  var password = "";
+  
+  // Generate random password
+  for (var i = 0; i < length; i++) {
+    password += allTypes.charAt(Math.floor(Math.random() * allTypes.length));
+  }
+  
+  passwordText.value = password;
+
 }
