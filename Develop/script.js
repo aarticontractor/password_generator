57d69fa99329845b03f480232c73f74f5fc93598
aarticontractor <contaaru@gmail.com>
2023-03-29 23:53:22 -0700

    added uppercase prompt

--- Develop/script.js
+++ Develop/script.js
@@ -11,24 +11,39 @@ function generatePassword() {
     alert("Password length cannot be empty.");
     return;
   }
+//   Added length prompt
   var length = parseInt(lengthPrompt);
   
   if (isNaN(length) || length < 8 || length > 128) {
     alert("Password length must be a number between 8 and 128.");
     return;
   }
+
+//   Added prompts for lowercase and uppercase characters
   var lowercasePrompt = prompt("Include lowercase letters? (yes/no)");
   var includeLowercase = lowercasePrompt.toLowerCase() === "yes";
   
+  var uppercasePrompt = prompt("Include uppercase letters? (yes/no)");
+  var includeUppercase = uppercasePrompt.toLowerCase() === "yes";
+
+
+
 
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   
   var allTypes = ""; 
+  
   // Include lowercase characters
   if (includeLowercase) {
     allTypes += lowercase;
   }
-  
+//   Include uppercase characters
+  if (includeUppercase) {
+    allTypes += uppercase;
+  }
+
+
 
 
   var password = "";
